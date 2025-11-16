c745e9461401534c3dd49ba39193e81749993534
slavagarchenko <cvgarchenko@gmail.com>
2025-11-16 23:56:49 +0700

    Update ex_16.py

--- ex_16.py
+++ ex_16.py
@@ -12,3 +12,10 @@ def ten_to_n(x, n):
         return digits[remainder]
     else:
         return ten_to_n(quotien, n) + digits[remainder]
+
+def ten_to_n_full(x, n):
+    """
+    """
+    if x == 0:
+        return "0"
+    return ten_to_n(x, n)
