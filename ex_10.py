9c60304b068c2a9b7dcfa01c2796bc487adeaf0a
slavagarchenko <cvgarchenko@gmail.com>
2025-11-17 09:32:02 +0700

    Update ex_10.py

--- ex_10.py
+++ ex_10.py
@@ -1,8 +1,18 @@
 def maxlist(a):
     """
+    Find the maximum element in a list using recursion.
+    
+    This function recursively finds the largest value in a list by comparing
+    the first element with the maximum of the rest of the list.
+
+    Args:
+        a (list): A list of comparable elements (numbers, strings, etc.)
+
+    Returns:
+        The maximum element in the list.
     """
     if len(a) == 1:
         return a[0]
     else:
         rest_max = maxlist(a[1:])
-        return a[0] if a[0] > rest_max else return rest_max
+        return a[0] if a[0] > rest_max else rest_max
