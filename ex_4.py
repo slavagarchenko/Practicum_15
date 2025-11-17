7d89c17a53148b7650a6cfb40cb8a2506e11d018
slavagarchenko <cvgarchenko@gmail.com>
2025-11-17 09:17:05 +0700

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -1,8 +1,16 @@
 def sum_progress(a1, r, n):
     """
+    Calculate the sum of the first n terms of a geometric progression.
+    
+    Parameters:
+    a1 (float): The first term of the geometric progression.
+    r (float):  The common ratio of the geometric progression.
+    n (int):    The number of terms to sum.
+    
+    Returns:
+    float:      The sum of the first n terms of the geometric progression.
     """
     if n == 1:
         return a1
     else:
         return progress(a1, r, n) + sum_progress(a1, r, n - 1)
-    
